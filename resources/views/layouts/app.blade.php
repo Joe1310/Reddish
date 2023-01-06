b05c64960643c5750a23ca3750816105f50527fb
Joe Henry <joseph1310@sky.com>
2023-01-06 13:57:00 +0000

    added new button to nav bar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,6 +19,9 @@
         <li class="nav-item active">
           <a class="nav-link" href="/home">Home</a>
         </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/winrates">Hero Winrates</a>
+        </li>
       @if (Auth::check())
         <!-- Display logout button for authenticated users -->
         <li class="nav-item">
