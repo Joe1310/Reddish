13b7c01311831c5d22e9b3e6e87586cfba4de9aa
Joe Henry <joseph1310@sky.com>
2023-01-07 20:12:57 +0000

    fixed admin error

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -22,7 +22,7 @@
             </div>
             <div style="text-align: left; display: flex; flex-direction: row;">
                 @if (Auth::check())
-                    @if (Auth::user()->id === $post->player_id)
+                    @if (Auth::user()->id === $post->player_id || Auth::user()->isAdmin())
                     <form method="POST" action="/posts/{{ $post->id }}">
                     @csrf
                     @method('DELETE')
